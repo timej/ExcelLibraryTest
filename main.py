14d57238ae0fd3c7ab7596ead2230f7ca9997716
Yasuhiro Niji <awoni@awoni.net>
2017-12-28 10:43:30 +0900

    openpyxl

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ def pdwrite(in_path, out_path):
     df = pd.read_csv(in_path, sep='\t', header=None)
     df.to_excel(out_path, header=False, index=False)
 
+def openpyxl_write(in_path, out_path):
+    from openpyxl import Workbook
+    from openpyxl.utils.dataframe import dataframe_to_rows
+
+    df = pd.read_csv(in_path, sep='\t', header=None)
+    
+    wb = Workbook()
+    ws = wb.active
+
+    for r in dataframe_to_rows(df, index=True, header=True):
+        ws.append(r)
+    wb.save(out_path)
+
+
 if __name__ == "__main__":
     import sys
     import os
@@ -21,5 +35,7 @@ if __name__ == "__main__":
     else:
         if '-x' in sys.argv:
             pdwrite('App_Data/DataIn/am0411.txt', 'App_Data/DataOut/px.xlsx')
+        elif '-o' in sys.argv:
+            openpyxl_write('App_Data/DataIn/am0411.txt', 'App_Data/DataOut/po.xlsx')
         else:
             pdwrite('App_Data/DataIn/am0411.txt', 'App_Data/DataOut/ps.xls')
