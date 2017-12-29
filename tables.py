683741507e4331af9ca1bb4ee3d4b35da74c00be
Yasuhiro Niji <awoni@awoni.net>
2017-12-29 14:16:55 +0900

    都道府県別の表ができるようにした

--- tables.py
+++ tables.py
@@ -3,36 +3,48 @@ from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.worksheet.table import Table, TableStyleInfo
 
-# http://openpyxl.readthedocs.io/en/default/pandas.html#
+def get_population(pref):
+    # http://openpyxl.readthedocs.io/en/default/pandas.html#
 
-path = 'App_Data/DataIn/population.txt'
-df = pd.read_csv(path, sep='\t')
+    path = 'App_Data/DataIn/population.txt'
+    df = pd.read_csv(path, sep='\t')
 
-wb = Workbook()
-ws = wb.get_active_sheet()
+    if pref != 0:
+        df = df[df['地域コード'] // 1000 == pref]
 
-for r in dataframe_to_rows(df, index=False, header=True):
-    ws.append(r)
+    wb = Workbook()
+    ws = wb.get_active_sheet()
 
-for i in range(2, len(df.index) + 1):
-    ws.cell(row=i, column=3).number_format = '#,##0'
-    ws.cell(row=i, column=4).number_format = '#,##0.00'
-    ws.cell(row=i, column=5).number_format = '#,##0.0'
-    ws.cell(row=i, column=6).number_format = '#,##0'
-ws.column_dimensions['B'].width = 20 
-ws.column_dimensions['C'].width = 12
-ws.column_dimensions['D'].width = 12
-ws.column_dimensions['E'].width = 12
-ws.column_dimensions['F'].width = 12
+    for r in dataframe_to_rows(df, index=False, header=True):
+        ws.append(r)
 
-# http://openpyxl.readthedocs.io/en/default/worksheet_tables.html
+    for i in range(2, len(df.index) + 1):
+        ws.cell(row=i, column=3).number_format = '#,##0'
+        ws.cell(row=i, column=4).number_format = '#,##0.00'
+        ws.cell(row=i, column=5).number_format = '#,##0.0'
+        ws.cell(row=i, column=6).number_format = '#,##0'
+    ws.column_dimensions['B'].width = 20 
+    ws.column_dimensions['C'].width = 12
+    ws.column_dimensions['D'].width = 12
+    ws.column_dimensions['E'].width = 12
+    ws.column_dimensions['F'].width = 12
 
-tab = Table(displayName="Table2", ref="A1:F" + str(len(df.index) + 1))
+    # http://openpyxl.readthedocs.io/en/default/worksheet_tables.html
 
-# Add a default style with striped rows and banded columns
-style = TableStyleInfo(name="TableStyleLight15", showFirstColumn=False,
-                       showLastColumn=False, showRowStripes=True, showColumnStripes=False)
-tab.tableStyleInfo = style
-ws.add_table(tab)
+    tab = Table(displayName="Table2", ref="A1:F" + str(len(df.index) + 1))
 
-wb.save('App_Data/DataOut/population1.xlsx')
+    # Add a default style with striped rows and banded columns
+    style = TableStyleInfo(name="TableStyleLight15", showFirstColumn=False,
+                        showLastColumn=False, showRowStripes=True, showColumnStripes=False)
+    tab.tableStyleInfo = style
+    ws.add_table(tab)
+
+    wb.save('App_Data/DataOut/population.xlsx')
+
+if __name__ == "__main__":
+    import sys
+    pref = 0
+    if len(sys.argv) > 1:
+        pref = int(sys.argv[1])
+    get_population(pref)
+    
