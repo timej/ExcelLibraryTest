f9c0863db51c967634138ba6433b47b16b3742b1
awoni <teku@awoni.net>
2018-03-01 17:48:25 +0900

    openpyxlでの読み込みの追加

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -4,9 +4,28 @@ def pdread(in_path, out_path):
     df = pd.read_excel(in_path, header=None)
     df.to_csv(out_path, header=False, index=False)
 
+
 def pdwrite(in_path, out_path):
     df = pd.read_csv(in_path, sep='\t', header=None)
     df.to_excel(out_path, header=False, index=False)
+
+
+def iter_row(row):
+    yield [cell.value for cell in row]
+
+
+def openpyxl_read(in_path, out_path):
+    from openpyxl import load_workbook
+    import csv
+
+    wb = load_workbook(in_path, read_only=True)
+    worksheet = wb.active
+
+    with open(out_path, 'w') as f:
+        writer = csv.writer(f, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
+
+        for row in worksheet.rows:
+            writer.writerows(iter_row(row))
 
 '''
 def openpyxl_write(in_path, out_path):
@@ -35,6 +54,8 @@ if __name__ == "__main__":
     if '-r' in sys.argv:
         if '-x' in sys.argv: 
             pdread('App_Data/DataIn/am0411.xlsx', 'App_Data/DataOut/px.txt')
+        elif '-o' in sys.argv:
+            openpyxl_read('App_Data/DataIn/am0411.xlsx', 'App_Data/DataOut/po.txt')
         else:
             pdread('App_Data/DataIn/am0411.xls', 'App_Data/DataOut/ps.txt')
     else:
