0ffa8b355b80799856060605835d2716785ee339
Yasuhiro Niji <awoni@awoni.net>
2018-03-01 20:08:32 +0900

    少しの修正

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 def pdread(in_path, out_path):
-    df = pd.read_excel(in_path, header=None)
+    df = pd.read_excel(in_path, sep='\t', header=None)
     df.to_csv(out_path, header=False, index=False)
-
 
 def pdwrite(in_path, out_path):
     df = pd.read_csv(in_path, sep='\t', header=None)
@@ -18,20 +17,6 @@ def openpyxl_read(in_path, out_path):
     from openpyxl import load_workbook
     import csv
 
-    wb = load_workbook(in_path, read_only=True)
-    worksheet = wb.active
-
-    with open(out_path, 'w') as f:
-        writer = csv.writer(f, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
-
-        for row in worksheet.rows:
-            writer.writerows(iter_row(row))
-
-'''
-def openpyxl_write(in_path, out_path):
-    df = pd.read_csv(in_path, sep='\t', header=None)
-    df.to_excel(out_path, header=False, index=False, engine='openpyxl')
-'''
 def openpyxl_write(in_path, out_path):
     from openpyxl import Workbook
     from openpyxl.utils.dataframe import dataframe_to_rows
@@ -54,8 +39,6 @@ if __name__ == "__main__":
     if '-r' in sys.argv:
         if '-x' in sys.argv: 
             pdread('App_Data/DataIn/am0411.xlsx', 'App_Data/DataOut/px.txt')
-        elif '-o' in sys.argv:
-            openpyxl_read('App_Data/DataIn/am0411.xlsx', 'App_Data/DataOut/po.txt')
         else:
             pdread('App_Data/DataIn/am0411.xls', 'App_Data/DataOut/ps.txt')
     else:
